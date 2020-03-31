87143a0783755243c1ede52c6747d9d42b300d34
Tanveer038 <62908503+Tanveer038@users.noreply.github.com>
2020-03-31 14:12:41 -0700

    Add files via upload

---
+++ Drones-Flying.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+import argparse
+class Node:
+    def __init__(self):
+        self.id = 0
+        self.pos = 0
+        self.vel = 0
+        self.cluster_head = 0
+class Cluster:
+    def __init__(self):
+        self.id = 0
+        self.level = 0
+class MultiagentSimulation(object):
+    def __init__(self, n_agents=50, interval=32,
+                 xmax=1, ymax=1):
+        # Parameters
+        self.n_agents = n_agents  # Number of agents
+        self.interval = interval  #
+        self.xmax = xmax
+        self.ymax = ymax
+        # State
+        self.pos_agents = (self.xmax, self.ymax) * np.random.rand(n_agents, 2)  # agent positions
+        self.vel_agents = 0.1 * np.random.randn(n_agents, 2)  # agent velocities
+        # Animation
+        self.fig, self.ax = plt.subplots()
+        self.scatter = self.ax.scatter([],[])
+        self.animation = animation.FuncAnimation(self.fig, self.update_plot, interval=self.interval,
+                                                 init_func=self.setup_plot, blit=True, frames=100)
+    def setup_plot(self):
+        self.ax.set_xlim((0, self.xmax))
+        self.ax.set_ylim((0, self.ymax))
+        self.scatter.set_offsets(self.pos_agents)
+        return [self.scatter]
+    def update_plot(self, i):
+        self.pos_agents += self.vel_agents * 0.001 * self.interval
+        self.pos_agents = np.remainder(self.pos_agents, (self.xmax, self.ymax))
+        self.scatter.set_offsets(self.pos_agents)
+        return [self.scatter]
+parser = argparse.ArgumentParser()
+parser.add_argument("-N", "--n-agents", action="store", type=int,
+                    dest="n_agents", default=50,
+                    help="number of agents to simulate")
+if __name__ == '__main__':
+    args = parser.parse_args()
+    ms = MultiagentSimulation(n_agents=args.n_agents)
+    ms.animation.save("out.gif", dpi=70, writer="imagemagick")
+    plt.show()
